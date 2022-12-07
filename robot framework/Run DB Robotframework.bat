b797db0895ce9935c6d82c98ad743fc0f4f87e9c
Manuele <manuele.salvador@bitrock.it>
2022-12-07 17:16:11 +0100

    add row to bat file

--- robot framework/Run DB Robotframework.bat
+++ robot framework/Run DB Robotframework.bat
@@ -1,3 +1,4 @@
 @echo "Start running MySql DB tests"
 robot -x junitresult.xml C:\Users\Manuele\AppData\Local\Programs\Python\Python39\Lib\site-packages\Robotframework-Database-Library-master\test\MySQL_DB_Tests.robot
 @echo "Finish running MySql DB tests"
+@echo "Exiting..."
